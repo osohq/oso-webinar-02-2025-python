ce029a2cab980a5941dc4082e1ea071de9d2d61b
Greg Sarjeant <1686767+gsarjeant@users.noreply.github.com>
2025-02-05 10:43:16 -0500

    Forgot to save authz_decorators.py

--- authz_decorators.py
+++ authz_decorators.py
@@ -1,8 +1,29 @@
+from typing import Dict
+from data import User
+from permissions import RBAC
 from flask import jsonify, request
 from functools import wraps
 from data import User
-from authz import has_permission, has_same_org, user_is_owner_if_in_sales
 from order_service import OrderService
+
+# Abstracted authorization logic
+
+def has_permission(user: User, permission: str):
+    user_role = user.role
+    permissions = [p.value for p in RBAC[user_role]]
+
+    return permission in permissions
+
+def has_same_org(user: User, order: Dict):
+    return user.org == order["org"]
+
+def user_is_owner_if_in_sales(user: User, order: Dict):
+    if user.role == "sales" and order["sold_by"] != user.username:
+        return False
+    
+    # Note: This assumes we don't enter this function with a user
+    # who has an invalid role for the action 
+    return True
 
 def require_permission(permission):
     def decorator(f):
